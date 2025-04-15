1303fae3a3dc23b75eb02e6bb4177968fd0b52bb
RenanFerreira02 <renankferreira01@gmail.com>
2025-04-15 10:48:57 -0300

    Section component adjustments and App renderization fixed

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -15,8 +15,9 @@ const Section = ({ className, id, crosses, crossesOffset, customPaddings, childr
       {crosses && (
         <>
           <div
-            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 pointer-events-none lg:block xl:left-10 right-10
-                ${crossesOffset && crossesOffset} `}
+            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
+              crossesOffset && crossesOffset
+            } pointer-events-none lg:block xl:left-10 right-10`}
           />
 
           <SectionSvg crossesOffset={crossesOffset} />
