d8d3e398656f5abb832d795e8cbdd738f74ace2b
RenanFerreira02 <renankferreira01@gmail.com>
2025-04-14 22:10:59 -0300

    Hero Section component completed

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,7 +4,7 @@ const Section = ({ className, id, crosses, crossesOffset, customPaddings, childr
   return (
     <div
       id={id}
-      className={`relative ${customPaddings || "py-10 lg:py-16 xl:py-20"} ${crosses ? "lg:py-32 xl:py-40" : ""} 
+      className={`relative ${customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`}
       ${className || ""}`}>
       {children}
 
@@ -15,7 +15,7 @@ const Section = ({ className, id, crosses, crossesOffset, customPaddings, childr
       {crosses && (
         <>
           <div
-            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 pointer-events-none lg:block xl:left-10 xl:right-10
+            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 pointer-events-none lg:block xl:left-10 right-10
                 ${crossesOffset && crossesOffset} `}
           />
 
